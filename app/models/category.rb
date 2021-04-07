a9ef19cb6307b27a27a5ff74add81f1869706232
kotaro-hetsugi <kotaro.hetsugi@gmail.com>
2021-04-07 16:25:57 +0900

    不要記述削除

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,19 +1,3 @@
 class Category < ApplicationRecord
   has_many :ideas
-
-  # with_options presence: true do
-  #   validates :name #category_name
-  # end
-
-  # validates :name, presence: true, uniqueness: true
-
-  # def save
-  # category = Category.where(name: name).first_or_initialize
-  # category.save
-  # Category.create(name: name)
-
-  # category = Category.where(name: name).first_or_initialize
-  # category.save
-
-  # end
 end
